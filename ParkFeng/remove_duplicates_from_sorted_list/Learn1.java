bb23848beffcc4d0d41e572283b5cccec6d6c03f
ParkFeng <1351315441@qq.com>
2019-01-17 00:05:02 +0800

    leetcode

---
+++ ParkFeng/remove_duplicates_from_sorted_list/Learn1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == NULL)
+            return NULL;
+        ListNode * temp = head;
+        while (temp -> next != NULL) {
+            if (temp -> val != temp -> next -> val) {
+                temp = temp -> next;
+            } else {
+                temp -> next = temp -> next -> next;
+            }
+        }
+        return head;
+    }
+}
